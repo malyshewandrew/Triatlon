344dc11434423dd66da02cea52198d4f3c2bcaf7
malyshewandrew <malyshewandrew@protonmail.com>
2024-06-11 01:08:53 +0300

    create philosophy view controller

--- Triathlon/UI/UIComponents.swift
+++ Triathlon/UI/UIComponents.swift
@@ -7,6 +7,8 @@ let cornerRadius: CGFloat = 16
 // MARK: - FONTS:
 
 let fontLightStandard12: UIFont = .systemFont(ofSize: 12, weight: .light, width: .standard)
+let fontMediumLight14: UIFont = .systemFont(ofSize: 14, weight: .light, width: .standard)
 let fontMediumStandard14: UIFont = .systemFont(ofSize: 14, weight: .medium, width: .standard)
 let fontBoldStandard16: UIFont = .systemFont(ofSize: 16, weight: .bold, width: .standard)
+let fontBoldStandard32: UIFont = .systemFont(ofSize: 32, weight: .bold, width: .standard)
 let fontBoldStandard64: UIFont = .systemFont(ofSize: 64, weight: .bold, width: .standard)
