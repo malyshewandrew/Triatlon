9b0aaf320e0c6d1d6f5b6f8ee2841a412903d223
malyshewandrew <malyshewandrew@protonmail.com>
2024-06-11 19:19:28 +0300

    create schedule view controller

--- Triathlon/VC/Schedule/ScheduleVC.swift
+++ Triathlon/VC/Schedule/ScheduleVC.swift
@@ -2,13 +2,13 @@ import UIKit
 
 // MARK: - protocol
 protocol ScheduleVCProtocol: AnyObject {
-    
 }
 
 class ScheduleVC: UIViewController {
     // MARK: - PROPERTIES:
     
     var presenter: SchedulePresenterProtocol!
+    private let titleLabel = UILabel()
     
     // MARK: - LIFYCYCLE:
     
@@ -22,24 +22,31 @@ class ScheduleVC: UIViewController {
     // MARK: - ADD SUBVIEWS:
     
     private func addSubviews() {
-        view.addSubviews()
+        view.addSubviews(titleLabel)
     }
     
     // MARK: - CONFIGURE CONSTRAINTS:
     
     private func configureConstraints() {
-        
+        // TITLE:
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 75).isActive = true
     }
     
     // MARK: - CONFIGURE UI:
     
     private func configureUI() {
         // VIEW:
-        view.backgroundColor = .colorBackground
+        view.backgroundColor = .colorTabBar
         
         // NAVIGATION CONTROLLER:
         navigationItem.title = ""
-
+        
+        // TITLE:
+        titleLabel.textColor = .white
+        titleLabel.text = "Расписание тренировок:"
+        titleLabel.font = fontBoldStandard16
     }
     
     // MARK: - HELPERS:
@@ -47,5 +54,4 @@ class ScheduleVC: UIViewController {
 
 // MARK: - EXTENSION:
 extension ScheduleVC: ScheduleVCProtocol {
-    
 }
